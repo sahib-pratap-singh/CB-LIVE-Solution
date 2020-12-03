ea8f3e011eabd930fc2c1a9fbd6734c82d1cd503
Sahib pratap singh <59799717+sahib-pratap-singh@users.noreply.github.com>
2020-12-03 09:37:03 +0530

    Update main.cpp

--- Sorting and searching/insertion sort/main.cpp
+++ Sorting and searching/insertion sort/main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-    int a[100000],no;
-    cin>>no;
-    for(int i=0;i<no;i++){
-        cin>>a[i];
+int main() {
+    int n;
+    cin>>n;
+    int a[1000];
+    for(int i=0;i<n;i++){
+            cin>>a[i];
     }
-    for(int i=0;i<no;i++){
-        for(int j=i-1;j>=0;j--){
-            if(a[j]>a[i]){
-                swap(a[j],a[i]);
+    for(int i=1;i<n;i++){
+            int key=a[i];
+            int j=i-1;
+            while(j>=0 and a[j]>key){
+                    a[j+1]=a[j];
+                    j--;
             }
-        }
+            a[j+1]=key;
     }
-    for(int i=0;i<no;i++){
-        cout<<a[i]<<" ";
+    for(int i=0;i<n;i++){
+            cout<<a[i]<<" ";
     }
-    return 0;
 }
